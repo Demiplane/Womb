d2e968946cb539ebd0866361fbdcb13e26267093
Isaac Zais <isaaczais@hotmail.com>
2018-07-02 22:32:33 -0500

    Switched DI container to use Simpleinjector

--- Womb/Startup.cs
+++ Womb/Startup.cs
@@ -5,9 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
 using Womb.NameGeneration;
 using Womb.Platform;
 using Womb.WordResolver;
@@ -16,6 +22,8 @@ namespace Womb
 {
     public class Startup
     {
+        private Container container = new Container();
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,20 +35,30 @@ namespace Womb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
-            services.AddSingleton<IWordResolver, NonCommonWordResolver>();
-            services.AddSingleton<IConfiguration>(Configuration);
+            
 
-            services.AddTransient<INameGenerator, DualWordNameGenerator>();
-            services.AddTransient<IConnectionStringResolver, ConfigurationConnectionStringResolver>();
-            services.AddTransient<ICreationCountRepository, SqlCreationCountRepository>();
-            services.AddTransient<ICharacterCreationCountResolver, SqlCharacterCreationCountResolver>();
-            services.AddTransient<ICharacterSaver, SqlCharacterSaver>();
+            IntegrateSimpleInjector(services);
+
+        }
+
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(new SimpleInjectorViewComponentActivator(container));
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeContainer(app);
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
@@ -60,5 +78,26 @@ namespace Womb
                     template: "{controller=Character}/{action=Birth}");
             });
         }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            container.RegisterInstance<IFileProvider>(new PhysicalFileProvider(Directory.GetCurrentDirectory()));
+            container.RegisterSingleton<IWordResolver, NonCommonWordResolver>();
+            container.RegisterInstance<IConfiguration>(Configuration);
+
+            container.Register<INameGenerator, DualWordNameGenerator>();
+            container.Register<IConnectionStringResolver, ConfigurationConnectionStringResolver>();
+            container.Register<ICreationCountRepository, SqlCreationCountRepository>();
+            container.Register<ICharacterCreationCountResolver, SqlCharacterCreationCountResolver>();
+            container.Register<ICharacterSaver, SqlCharacterSaver>();
+
+            // Allow Simple Injector to resolve services from ASP.NET Core.
+            container.AutoCrossWireAspNetComponents(app);
+        }
     }
 }
+
