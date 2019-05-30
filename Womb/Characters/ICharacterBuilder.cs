cecd92fae349539063c447b60ea858bcbf4e0f92
Isaac Zais <isaac.zais@chrobinson.com>
2019-05-30 11:05:45 -0500

    Builder interface

---
+++ Womb/Characters/ICharacterBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Womb.Models;
+
+namespace Womb.Characters
+{
+    public interface ICharacterBuilder
+    {
+        Character BuildCharacter();
+    }
+}
